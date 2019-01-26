81aa68e8b2e60317153e7e08633c8db5f9591f1d
maquirag <maquirag@gmail.com>
2019-01-26 21:47:43 +0100

    ASCII Circles (sololearn version)

--- circle.py
+++ circle.py
@@ -1,18 +1,29 @@
 """
-Drawing a circle with radius N on char
+BLACK HOLE
+Enter the radius of the hole!
+MIN = 1
+MAX = 10 for Portrait, 18 for Landscape
 """
 #import sys
 #from codecs import getwriter
 #sys.stdout = getwriter('utf_16')(sys.stdout.buffer, 'strict')
-T = '\N{WHITE LARGE SQUARE}'
+W = '\N{WHITE LARGE SQUARE}'
+B = '\N{BLACK LARGE SQUARE}'
 
 try:
-    r = int(input('Enter radius: '))
-    assert 4 < r < 50
+    r = int(input())
+    assert 0 < r < 19
 except (ValueError, AssertionError):
     r = 10
 
 for x in range(-r, r + 1):
     for y in range(-r, r + 1):
         d = (x**2 + y**2)**0.5
-        print('*' if d >= r else ' ', sep='', end='' if y < r else '\n')
+        print(W if d >= r else B, sep='', end='' if y < r else '\n')
+
+print('')
+
+for x in range(-r, r + 1):
+    for y in range(-r, r + 1):
+        d = (x**2 + y**2)**0.5
+        print(B if abs(d - r) < 0.5 else W, sep='', end='' if y < r else '\n')
