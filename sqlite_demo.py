0ee431fd227c582d318a5d06ffbcb7cf2a100147
maquirag <maquirag@gmail.com>
2019-01-29 10:28:57 +0100

    sqlite

---
+++ sqlite_demo.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+with sqlite3.connect(':memory:') as memdb:
+    cur = memdb.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='info'")
+    if not cur.fetchall():
+        print('Table does not exist. Creating table.')
+        cur.execute("CREATE TABLE info (name TEXT, age INTEGER)")
+    # Insert some rows
+    newrows = [('Joe', 15), ('Jack', 22)]
+    cur.executemany("INSERT INTO info VALUES (?, ?)", newrows)
+    memdb.commit()
+    # Query the table and print rows
+    cur.execute('SELECT * FROM info')
+    print(*cur.fetchall(), sep='\n')
