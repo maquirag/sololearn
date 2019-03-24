f2196a2a618bbc185470bec0911b97c3ef75aec2
maquirag <maquirag@gmail.com>
2019-03-24 13:20:49 +0100

    Josephus Problem

--- josephus.py
+++ josephus.py
@@ -1,12 +1,12 @@
 """ Josephus Problem
+INSTRUCTION: Enter number of soldiers > 1
+             or leave empty for default 10.
 The problem is named after Flavius Josephus, a Jewish historian living in the 1st century.
 According to Josephus' account of the siege of Yodfat, he and his 40 soldiers were trapped
 in a cave by Roman soldiers. They chose suicide over capture, and settled on a serial method
 of committing suicide by drawing lots. Josephus states that by luck or possibly by the hand of God,
 he and another man remained until the end and surrendered to the Romans rather than killing themselves.
 Source: Wikipedia
-INSTRUCTION: Enter number of soldiers > 1
-             or leave empty for default 10.
 """
 def josephus(circle):
     if len(circle) == 1:
